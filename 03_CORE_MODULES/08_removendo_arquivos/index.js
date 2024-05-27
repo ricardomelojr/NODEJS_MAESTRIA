7977747e545557603d01b4cdf400e597d648c74e
Ricardo Melo Jr <ricardomelojunior@gmail.com>
2024-05-27 17:02:54 -0400

    Remover arquivos

--- 03_CORE_MODULES/08_removendo_arquivos/index.js
+++ 03_CORE_MODULES/08_removendo_arquivos/index.js
@@ -1,3 +1,31 @@
+// Node.js program to demonstrate the
+// fs.unlink() method
+
+// Import the filesystem module
 const fs = require('fs');
 
-fs.unlink;
+// Get the files in current directory
+// before deletion
+getFilesInDirectory();
+
+// Delete example_file.txt
+fs.unlink('arquivo.txt', err => {
+  if (err) console.log(err);
+  else {
+    console.log('\nDeleted file: arquivo.txt');
+
+    // Get the files in current directory
+    // after deletion
+    getFilesInDirectory();
+  }
+});
+
+// Function to get current filenames
+// in directory with specific extension
+function getFilesInDirectory() {
+  console.log('\nFiles present in directory:');
+  let files = fs.readdirSync(__dirname);
+  files.forEach(file => {
+    console.log(file);
+  });
+}
