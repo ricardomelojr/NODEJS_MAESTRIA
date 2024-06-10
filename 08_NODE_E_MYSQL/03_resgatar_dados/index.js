086688efa106525f39de9a00c667dff475ad1916
Ricardo Melo Jr <ricardomelojunior@gmail.com>
2024-06-10 16:37:22 -0400

    Data Update

---
+++ 08_NODE_E_MYSQL/03_resgatar_dados/index.js
@@ -0,0 +1,85 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import mysql from 'mysql';
+
+const port = 3000;
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.post('/books/insertbook', (req, res, next) => {
+  const title = req.body.title;
+  const pageqty = parseInt(req.body.pageqty);
+
+  console.log('Title:', title);
+  console.log('Page Quantity:', pageqty);
+
+  if (isNaN(pageqty)) {
+    console.error('Invalid page quantity:', pageqty);
+    return res.status(400).send('Invalid page quantity');
+  }
+
+  const sql = 'INSERT INTO books (title, pageqty) VALUES (?, ?)';
+
+  connection.query(sql, [title, pageqty], function (err) {
+    if (err) {
+      console.error('Error inserting data:', err);
+      return res.status(500).send('Error inserting data');
+    }
+
+    res.redirect('/');
+  });
+});
+
+app.get('/books', (req, res, next) => {
+  const sql = 'SELECT * FROM books';
+  connection.query(sql, (err, data) => {
+    if (err) {
+      console.error('Erro ao resgatar informações:', err);
+      return res.status(500).send('Error get data');
+    }
+
+    const books = data;
+    console.log(books);
+    res.render('books', { books });
+  });
+});
+
+// Configura a conexão
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodemysql2',
+});
+
+// Realiza a conexão
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+
+  app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`);
+  });
+});
---
+++ 08_NODE_E_MYSQL/03_resgatar_dados/public/script.js
@@ -0,0 +1,8 @@
+// Seleciona o botão pelo ID
+const fazOLButton = document.getElementById('fazOLButton');
+
+// Adiciona um event listener para o evento de clique
+fazOLButton.addEventListener('click', () => {
+  // Redireciona para a outra página
+  window.location.href = '/books';
+});
