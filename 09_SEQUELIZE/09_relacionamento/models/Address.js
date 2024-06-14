a3c193847d6bb877d3e72a764fdb1918515528ba
Ricardo Melo Jr <ricardomelojunior@gmail.com>
2024-06-14 18:06:15 -0400

    Address

--- 09_SEQUELIZE/09_relacionamento/models/Address.js
+++ 09_SEQUELIZE/09_relacionamento/models/Address.js
@@ -2,20 +2,26 @@ import { Sequelize, DataTypes } from 'sequelize';
 import sequelize from '../db/conn.js';
 import User from './User.js';
 
-const Address = sequelize.define('Address', {
-  street: {
-    type: DataTypes.STRING,
-    required: true,
+const Address = sequelize.define(
+  'Address',
+  {
+    street: {
+      type: DataTypes.STRING,
+      required: true,
+    },
+    number: {
+      type: DataTypes.STRING,
+      required: true,
+    },
+    city: {
+      type: DataTypes.STRING,
+      required: true,
+    },
   },
-  number: {
-    type: DataTypes.STRING,
-    required: true,
-  },
-  city: {
-    type: DataTypes.STRING,
-    required: true,
-  },
-});
+  {
+    tableName: 'Address',
+  }
+);
 
 Address.belongsTo(User);
 
