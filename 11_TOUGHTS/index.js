072ceb1081c2e751b673d4dc42bce3c6de57ce71
Ricardo Melo Jr <ricardomelojunior@gmail.com>
2024-06-21 16:03:38 -0400

    Toughts start

---
+++ 11_TOUGHTS/index.js
@@ -0,0 +1,73 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import session from 'express-session';
+import SessionFileStore from 'session-file-store';
+import flash from 'express-flash';
+import conn from './db/conn.js';
+import path from 'path';
+import os from 'os';
+
+const app = express();
+const FileStore = SessionFileStore(session);
+
+// Configuração do motor de visualização
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// Middleware para analisar o corpo das requisições
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Configuração da sessão
+app.use(
+  session({
+    name: 'session',
+    secret: 'nosso_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: path.join(os.tmpdir(), 'sessions'),
+    }),
+    cookie: {
+      secure: false, // Defina como true se estiver usando HTTPS
+      maxAge: 360000, // Duração da sessão em milissegundos
+      expires: new Date(Date.now() + 360000), // Data de expiração do cookie
+      httpOnly: true, // O cookie só será acessível pelo protocolo HTTP
+    },
+  })
+);
+
+// Middleware para mensagens flash
+app.use(flash());
+
+//public path
+app.use(express.static('public'));
+
+// se session to res
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+  next();
+});
+
+// Rota principal
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+// Conexão com o banco de dados e inicialização do servidor
+const port = 3000;
+
+conn
+  .sync()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Aplicativo escutando na porta ${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('Erro ao sincronizar com o banco de dados:', err);
+  });
