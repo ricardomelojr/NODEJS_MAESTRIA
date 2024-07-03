b87e8b435a7b25e1be3beba6af68e84042781972
Ricardo Melo <ricardomelojunior@gmail.com>
2024-07-03 16:56:30 -0400

    Mongoose

---
+++ 13_MONGOOSE/01_instalacao/routes/productsRoutes.js
@@ -0,0 +1,18 @@
+/* productRoutes.js */
+import express from 'express';
+const router = express.Router();
+
+import ProductController from '../controllers/ProductController.js';
+
+/* GET */
+router.get('/create', ProductController.createProduct);
+router.get('/', ProductController.showProducts);
+router.get('/:id', ProductController.getProduct);
+router.get('/edit/:id', ProductController.editProduct);
+
+/* POST */
+router.post('/remove/:id', ProductController.removeProduct);
+router.post('/create', ProductController.createProductPost);
+router.post('/edit', ProductController.editProductPost);
+
+export default router;
