531953d3803c11b36415f01e47a8049766c14626
ricardomelojr <ricardomelojunior@gmail.com>
2024-07-02 21:24:25 -0400

    Atualizando o diretório de testes

--- 00_TESTE/08_TOGGLE_CONTENT/script.js
+++
@@ -1,8 +0,0 @@
-function toggleContent() {
-    var content = document.getElementById("content");
-    if (content.style.display === "block") {
-        content.style.display = "none";
-    } else {
-        content.style.display = "block";
-    }
-}
