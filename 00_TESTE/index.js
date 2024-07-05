362c3cd5aa6e79d75d3ecf768f4c789fe4a74789
Ricardo Melo <ricardomelojunior@gmail.com>
2024-07-05 16:29:38 -0400

    API Finish

--- 00_TESTE/index.js
+++ 00_TESTE/index.js
@@ -1,3 +1,15 @@
-let numero = '10.5';
-let inteiro = parseInt(numero);
-console.log(`Resultado: ${inteiro}`);
+const axios = require('axios');
+
+const apiKey = 'sua_chave_da_api';
+const city = 'São Paulo';
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${manaus}&appid=${apiKey}&units=metric`;
+
+axios
+  .get(url)
+  .then(response => {
+    const weatherData = response.data;
+    console.log(`A temperatura atual em ${city} é ${weatherData.main.temp}°C`);
+  })
+  .catch(error => {
+    console.error('Erro ao obter dados do clima:', error);
+  });
---
+++ 14_API/01_primeira_api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+/* ROTAS - ENDPOINTS */
+app.get('/', (req, res) => {
+  res.json({ message: 'Primeira rota criada com sucesso!' });
+});
+
+app.listen(3000);
