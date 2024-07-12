335a794fd642705dfbdf4582c21e123006d1c34e
Ricardo Melo <ricardomelojunior@gmail.com>
2024-07-12 15:27:07 -0400

    Footer Finish

---
+++ 15_GET_A_PET/frontend/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+/* COMPONENTS */
+import Navbar from './components/layout/Navbar.js';
+import Footer from './components/layout/Footer.js';
+
+/* PAGES */
+import Login from './components/pages/Auth/Login.js';
+import Register from './components/pages/Auth/Register.js';
+import Home from './components/pages/Home.js';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
