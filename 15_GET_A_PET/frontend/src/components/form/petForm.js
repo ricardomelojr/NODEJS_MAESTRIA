a27ee9f9cb28db91ebe31b6af33df6199ff8e8fe
Ricardo Melo <ricardomelojunior@gmail.com>
2024-07-31 16:47:11 -0400

    PetForm

--- 15_GET_A_PET/frontend/src/components/form/petForm.js
+++ 15_GET_A_PET/frontend/src/components/form/petForm.js
@@ -1,17 +1,34 @@
 import { useState } from 'react';
 import formStyles from './Form.module.css';
 import Input from './Input';
+import Select from './Select';
 
 function PetForm({ handleSugmit, petData, btnText }) {
   const [pet, setPet] = useState(petData || {});
-  const { preview, setPreview } = useState({});
+  const { preview, setPreview } = useState([]);
   const colors = ['Branco', 'Preto', 'Cinza', 'Caramelo', 'Mesclado'];
 
-  function onFileChange(e) {}
-  function handleChange(e) {}
+  function onFileChange(e) {
+    setPet({ ...pet, images: [...e.target.files] });
+  }
+  /* 
+    e.target.name => nome da chave
+    e.target.value => valor do input
+    */
+  function handleChange(e) {
+    setPet({ ...pet, [e.target.name]: e.target.value });
+  }
+  function handleColor(e) {
+    setPet({ ...pet, color: e.target.options[e.target.selectedIndex].text });
+  }
+  function submit(e) {
+    e.preventDefault();
+    console.log(pet);
+    //handleSubmit(pet);
+  }
 
   return (
-    <form className={formStyles.form_container}>
+    <form onSubmit={submit} className={formStyles.form_container}>
       <Input
         text="Imagens do Pet"
         type="file"
@@ -27,6 +44,30 @@ function PetForm({ handleSugmit, petData, btnText }) {
         handleOnChange={handleChange}
         value={pet.name || ''}
       />
+      <Input
+        text="Idade do Pet"
+        type="text"
+        name="age"
+        placeholder="Digite a idade"
+        handleOnChange={handleChange}
+        value={pet.age || ''}
+      />
+      <Input
+        text="Peso do Pet"
+        type="number"
+        name="weight"
+        placeholder="Digite o peso"
+        handleOnChange={handleChange}
+        value={pet.weight || ''}
+      />
+      <Select
+        name="coloar"
+        text="Selecionar a cor"
+        options={colors}
+        handleOnChange={handleColor}
+        value={pet.color || ''}
+      />
+      <input type="submit" value={btnText} />
     </form>
   );
 }
