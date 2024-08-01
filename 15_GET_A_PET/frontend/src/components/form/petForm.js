6327d0de114ad8baf74940f71532b1eec02d39e5
Ricardo Melo <ricardomelojunior@gmail.com>
2024-08-01 16:24:50 -0400

    Preview

--- 15_GET_A_PET/frontend/src/components/form/petForm.js
+++ 15_GET_A_PET/frontend/src/components/form/petForm.js
@@ -3,32 +3,37 @@ import formStyles from './Form.module.css';
 import Input from './Input';
 import Select from './Select';
 
-function PetForm({ handleSugmit, petData, btnText }) {
+function PetForm({ handleSubmit, petData, btnText }) {
   const [pet, setPet] = useState(petData || {});
-  const { preview, setPreview } = useState([]);
+  const [preview, setPreview] = useState([]);
   const colors = ['Branco', 'Preto', 'Cinza', 'Caramelo', 'Mesclado'];
 
   function onFileChange(e) {
     setPet({ ...pet, images: [...e.target.files] });
   }
-  /* 
-    e.target.name => nome da chave
-    e.target.value => valor do input
-    */
+
   function handleChange(e) {
     setPet({ ...pet, [e.target.name]: e.target.value });
   }
+
   function handleColor(e) {
     setPet({ ...pet, color: e.target.options[e.target.selectedIndex].text });
   }
+
   function submit(e) {
     e.preventDefault();
     console.log(pet);
-    //handleSubmit(pet);
+    handleSubmit(pet);
   }
 
   return (
     <form onSubmit={submit} className={formStyles.form_container}>
+      <div>
+        {preview.length > 0
+          ? preview.map((image, index) => <p key={index}>teste</p>)
+          : pet.images &&
+            pet.images.map((image, index) => <p key={index}>teste</p>)}
+      </div>
       <Input
         text="Imagens do Pet"
         type="file"
@@ -61,7 +66,7 @@ function PetForm({ handleSugmit, petData, btnText }) {
         value={pet.weight || ''}
       />
       <Select
-        name="coloar"
+        name="color"
         text="Selecionar a cor"
         options={colors}
         handleOnChange={handleColor}
