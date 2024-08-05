6d15b42d197319b21106e50819e80a15db010692
Ricardo Melo <ricardomelojunior@gmail.com>
2024-08-05 16:52:47 -0400

    MyPets

--- 15_GET_A_PET/frontend/src/components/pages/Pet/MyPets.js
+++ 15_GET_A_PET/frontend/src/components/pages/Pet/MyPets.js
@@ -1,18 +1,70 @@
+import api from '../../../utils/api';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import RoundedImage from '../../layout/RoundedImage';
+import useFlashMessage from '../../../hooks/useFlashMessage';
+import styles from './Dashboard.module.css';
 
 function MyPets() {
   const [pets, setPets] = useState([]);
+  const [token] = useState(localStorage.getItem('token') || '');
+  const { setFlashMessage } = useFlashMessage();
+
+  useEffect(() => {
+    api
+      .get('/pets/mypets', {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then(response => {
+        setPets(response.data.pets);
+      });
+  }, [token]);
+
+  async function removePet(id) {
+    let msgType = 'success';
+    const data = await api.delete(`/pets/${id}`, {
+      headers:{
+        Authorization: 
+      }
+    });
+  }
 
   return (
     <section>
-      <div>
+      <div className={styles.petlist_header}>
         <h1>MyPets</h1>
         <Link to="/pet/add">Cadastrar Pet</Link>
       </div>
-      <div>
+      <div className={styles.petlist_container}>
         {pets.length > 0 ? (
-          <p>Meus Pets cadastrados</p>
+          //<p>Meus Pets cadastrados</p>
+          pets.map(pet => (
+            <div className={styles.petlist_row} key={pet._id}>
+              <RoundedImage
+                src={`http://localhost:5000/images/pets/${pet.images[0]}`}
+                alt={pet.name}
+                width="px75"
+              />
+              <span className="bold">{pet.name}</span>
+              <div className={styles.actions}>
+                {pet.available ? (
+                  <>
+                    {pet.adopter && (
+                      <button className={styles.conclude_btn}>
+                        Concluir adoção
+                      </button>
+                    )}
+                    <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
+                    <button>Exluir</button>
+                  </>
+                ) : (
+                  <p>Pet já adotado</p>
+                )}
+              </div>
+            </div>
+          ))
         ) : (
           <p>Não há Pets Cadastrados</p>
         )}
